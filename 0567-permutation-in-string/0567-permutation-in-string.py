bd96f109f887134df59a63d1952d251385e343a0
Aastha Malik <aasthamalik1810@gmail.com>
2025-09-05 15:03:39 +0530

    Time: 46 ms (39.4%), Space: 18 MB (39.65%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,10 +3,22 @@ class Solution:
         if len(s1) > len(s2):
             return False
         l = len(s1)
-        new_s1 = sorted(s1)
-        for i in range(len(s2)):
-            sub_str = sorted(s2[i:i+l])
-            if new_s1 == sub_str :
+        
+        s1_counter = Counter(s1)
+        window_counter = Counter(s2[:l])
+
+        if s1_counter == window_counter:
+            return True
+        
+        for i in range(l, len(s2)):
+            left_ind = i - l
+            right_ind = i
+            
+            window_counter[s2[left_ind]] -= 1
+            window_counter[s2[right_ind]] += 1
+
+            if s1_counter == window_counter:
                 return True
         return False
+
         
