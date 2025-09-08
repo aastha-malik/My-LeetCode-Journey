df8b01438974b889990bd9081c034861d8346005
Aastha Malik <aasthamalik1810@gmail.com>
2025-09-08 20:02:28 +0530

    Time: 3 ms (95.26%), Space: 18.1 MB (36.35%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,13 +1,9 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        d = {}
         if len(set(s)) != len(set(t)):
             return False
         else:
-            for i in range(len(s)):
-                d[s[i]] = t[i]
+            d = dict(zip(s, t))
             res = 0
             for i in range(len(s)):
                 if d[s[i]] == t[i]:
