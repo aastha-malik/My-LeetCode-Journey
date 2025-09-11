e707c086aedd27a9b54f03eb7056c66f3c12f330
Aastha Malik <aasthamalik1810@gmail.com>
2025-09-11 20:30:06 +0530

    Time: 74 ms (9.79%), Space: 18.3 MB (13.19%) - LeetHub

--- 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.py
+++ 1045-check-if-word-is-valid-after-substitutions/1045-check-if-word-is-valid-after-substitutions.py
@@ -1,9 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         l = s.count("a")
-        for i in range(l):
-            if "abc" in s:
-                s = s.replace("abc", "")
-        return s == ""
+        i = 0
+        while i < len(s):
+            if s[i:i+3] == "abc":
+                s = s[:i] + s[i+3:]
+
+            elif s[i-1:i+2] == "abc":
+                s = s[:i-1] + s[i+2:]
+
+            elif s[i-2:i+1] == "abc":
+                s = s[:i-2] + s[i+1:]
+
+            elif s[i-3:i] == "abc":
+                s = s[:i-3] + s[i:]
+            
+            else:
+                i+= 1
+
+            if s == "abc":
+                return True
+            
+        return s ==""
 
         
