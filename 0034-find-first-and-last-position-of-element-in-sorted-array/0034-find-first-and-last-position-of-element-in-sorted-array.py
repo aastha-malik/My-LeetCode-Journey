4b76f77d5122d094d507b17aa8eccb9a1b6a728e
Aastha Malik <aasthamalik1810@gmail.com>
2025-09-16 18:34:35 +0530

    Time: 0 ms (100%), Space: 18.9 MB (75.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,19 +1,47 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         l = len(nums)
-        res = []
+        low = 0
+        high = l - 1
         if target not in nums:
             return [-1, -1]
         else:
-            for i in range(l):
-                if nums[i] == target:
-                    if len(res) <= 1 :
-                        res.append(i)
+            res = []
+
+            #First Position
+            while low <= high:
+                mid = (low + high) // 2
+
+                if target == nums[mid]:
+                    if len(res) == 0:
+                        res.append(mid)
+                        high = mid - 1
                     else:
-                        if i > res[-1]:
-                            res[-1] = i
-        if len(res) == 1:
-            return [res[0], res[0]]
-        return res
-                    
+                        if res[0] > mid:
+                            res[0] = mid
+                            high = mid - 1
+                elif target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            #Last Position
+            low = 0
+            high = l-1
+            while low <= high:
+                mid = (low + high) // 2
+
+                if target == nums[mid]:
+                    if len(res) == 1:
+                        res.append(mid)
+                        low = mid + 1
+                    else:
+                        if res[-1] < mid:
+                            res[-1] = mid
+                            low = mid + 1
+                elif target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return res
+
             
