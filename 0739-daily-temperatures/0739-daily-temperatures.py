c2501f4a7772b0dfcce41307025f632208eaa659
Aastha Malik <aasthamalik1810@gmail.com>
2026-03-29 15:29:40 +0530

    Time: 130 ms (15.29%), Space: 28.7 MB (45.77%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -4,14 +4,13 @@ class Solution:
         res = [0] * len(temperatures)
         i = 0
         while i < len(temperatures) :
-            if len(stack) != 0:
-                while len(stack) > 0 :
-                    x = stack[-1]
-                    if temperatures[x] < temperatures[i]:
-                        res[x] = i-x
-                        stack.pop()
-                    else:
-                        break
+            while len(stack) > 0 :
+                x = stack[-1]
+                if temperatures[x] < temperatures[i]:
+                    res[x] = i-x
+                    stack.pop()
+                else:
+                    break
 
             stack.append(i)   
             i+=1
