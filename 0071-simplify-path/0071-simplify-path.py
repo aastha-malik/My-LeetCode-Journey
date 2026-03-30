54345fe50bb3a042e7ea52fa7b3019460719d219
Aastha Malik <aasthamalik1810@gmail.com>
2026-03-30 16:37:52 +0530

    Time: 3 ms (40.61%), Space: 19.3 MB (78.46%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -3,13 +3,15 @@ class Solution:
         curr_dir = "."
         parent_dir = ".."
         s = path.split("/")
-        while "" in s:
-            s.remove("")
+        # while "" in s:
+        #     s.remove("")
         
         stack = []
         i = 0
         while i < len(s):
-            if s[i] == curr_dir:
+            if s[i] == "":
+                i+=1
+            elif s[i] == curr_dir:
                 i+=1
             elif s[i] == parent_dir:
                 if len(stack) != 0:
