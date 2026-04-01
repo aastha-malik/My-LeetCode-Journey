e20e10409bd24c11b2c4f9842ad123309cc3b613
Aastha Malik <aasthamalik1810@gmail.com>
2026-04-01 21:26:31 +0530

    Time: 3 ms (3.31%), Space: 19.5 MB (59.45%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,36 +1,18 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        res=[]
+        for i in matrix:
+            for j in i:
+                res.append(j)
         low = 0
-        high = len(matrix) - 1
-        n = len(matrix[0])
-        l = 0
-        while low <= high:
-            mid = (low+high) // 2
-            if matrix[mid][0] <= target <= matrix[mid][-1]:
-                l = 0
-                h = len(matrix[mid]) - 1
-                while l <= h:
-                    m = (l+h)//2
-                    if matrix[mid][m] == target:
-                        return True
-                    elif matrix[mid][m] < target:
-                        l = m + 1
-                    else:
-                        h = m - 1
-                return l < len(matrix[low]) and matrix[low][l] == target
-            elif target > matrix[mid][-1]:
-                low = mid + 1
-            else:
+        high = len(res) -1
+        while low < high:
+            mid = (low + high) //2
+            if res[mid] == target:
+                return True
+            elif res[mid] > target:
                 high = mid - 1
-        # l = 0
-        # h = len(matrix[low]) - 1
-        # while l <= h:
-        #     m = (l+h)//2
-        #     if matrix[low][m] == target:
-        #         return True
-        #     elif matrix[low][m] < target:
-        #         l = m + 1
-        #     else:
-        #         h = m - 1
-        return False
+            else: 
+                low = mid + 1
+        return res[low] == target
         
