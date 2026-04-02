b6ede2927c412b91aaef0752f3fcee4c8b7738d8
Aastha Malik <aasthamalik1810@gmail.com>
2026-04-02 22:50:28 +0530

    Time: 11 ms (4.11%), Space: 19.2 MB (93.67%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,16 +1,17 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        x = str(n)
-        for i in range(10):
-            l = len(x)
-            s = 0
-            for j in range(l):
-                a = int(x[j]) ** 2
-                s += a
-            if s == 1:
+        n = str(n)
+        arr = list(n)
+        i = 0
+        while i < 100:
+            
+            res = 0
+            for j in arr:
+                x = int(j)**2
+                res += x
+            arr = list(str(res))
+            if res == 1:
                 return True
-            else:
-                x = str(s)
+            i+=1
         return False
-
         
