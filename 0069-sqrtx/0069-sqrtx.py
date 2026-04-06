fa8dd8c6d12f1ef2879405d373f13ad51bb22ad9
Aastha Malik <aasthamalik1810@gmail.com>
2026-04-06 22:20:11 +0530

    Time: 7 ms (31.66%), Space: 19.4 MB (40.46%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,8 +1,22 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        root = str(x ** (0.5))
-        for i in range(len(root)):
-            if root[i] == ".":
-                return int(root[:i])
-        return int(root)
-        
+        if x == 0 or x == 1:
+            return x
+        low = 0
+        high = x
+        while low <= high:
+            mid = (low + high) // 2
+            if mid**2 == x:
+                return mid
+            elif mid*mid < x:
+                s = mid+1
+                if s**2 > x:
+                    return mid
+                else:
+                    low = mid+1
+            else:
+                s = mid-1
+                if s**2 < x:
+                    return mid-1
+                else:
+                    high = mid-1
