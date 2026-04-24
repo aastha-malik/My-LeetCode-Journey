b7fc175280c48dc2ea0e8b0ec4249b7a5d21170d
Aastha Malik <aasthamalik1810@gmail.com>
2026-04-24 20:37:17 +0530

    Time: 1331 ms (7.75%), Space: 22.3 MB (99.17%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,40 +1,29 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-
         if len(lists) > 0:
-
-            res = lists[0]
-
+            dummy = lists[0]
             for i in range(1, len(lists)):
-                dummy = ListNode(0)
-                ll = dummy
+                head = ListNode(0)
+                ll = head
+                node = lists[i]
+                while dummy and node:
 
-                first = res
-                second = lists[i]
+                    if dummy.val > node.val:
+                        ll.next = node
+                        node = node.next
 
-                while first and second :
-                    
-                    if first.val <= second.val:
-                        ll.next = first
-                        first = first.next
-
-                    else:
-                        ll.next = second
-                        second = second.next
+                    elif dummy.val <= node.val:
+                        ll.next = dummy
+                        dummy = dummy.next
                     ll = ll.next
-
-                if first :
-                    ll.next = first
-                if second :
-                    ll.next = second
-
-                res = dummy.next
-            return res
-            
-            
+                if dummy:
+                    ll.next = dummy
+                if node:
+                    ll.next = node
+                dummy = head.next
+            return dummy
