1ede13950d8284dbd0d8659e2c6ece32fdb4ff03
Aastha Malik <aasthamalik1810@gmail.com>
2026-04-29 20:32:37 +0530

    Time: 3 ms (2.22%), Space: 19.4 MB (6.84%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -7,10 +7,36 @@
 from collections import deque
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is not None:
+            return False
+        if p is not None and q is None:
+            return False
         if p is None and q is None:
             return True
-        if p is None or q is None:
-            return False
-        if p.val != q.val:
-            return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            p_deque = deque([p])
+            q_deque = deque([q])
+
+            while p_deque and q_deque:
+                node_p = p_deque.popleft()
+                node_q = q_deque.popleft()
+
+                if node_p.val != node_q.val:
+                    return False
+                if node_p.left and node_q.left:
+                    p_deque.append(node_p.left)
+                    q_deque.append(node_q.left)
+                if node_p.left is None and node_q.left is not None:
+                    return False
+                if node_p.left is not None and node_q.left is None:
+                    return False
+                
+                if node_p.right and node_q.right:
+                    p_deque.append(node_p.right)
+                    q_deque.append(node_q.right)
+                if node_p.right is None and node_q.right is not None:
+                    return False
+                if node_p.right is not None and node_q.right is None:
+                    return False
+            return True
+                
